4ad168179e10c95e3d33b8f1f2c0b909139252bf
mahabubul-alam <mahabubul.alam.faisal@gmail.com>
2021-01-20 21:19:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,13 @@ import os
 from CompileQAOA import Compile_QAOA_Qiskit as CQQ
 
 
-#create a parser object 
 parser = argparse.ArgumentParser(allow_abbrev=True)
-#add possible arguments
 parser.add_argument("-device_json", help="Target Device Configuration File", type=str, default=None, action='store', dest='QCD')
 parser.add_argument("-circuit_json", help="Problem QAOA ZZ Interaction File", type=str, default=None, action='store', dest='CKT')
 parser.add_argument("-config_json", help="QAOA/Compiler Configuration File", type=str, default=None, action='store', dest='CON')
-
 parser.add_argument("-policy_compilation", help="Chosen Compilation Policy: Instruction Parallelization-only (IP), Iterative Compilation (IterC), Incremental Compilation (IC), Variation-aware Incremental Compilation (VIC)", type=str, default='IC', action='store', dest='POL')
 parser.add_argument("-target_IterC", help="Iterative Compilation (IterC) should minimize: Depth (D) or Two-qubit gate-count (GC_2Q) or Estimated Success Probability (ESP)", type=str, default='GC_2Q', action='store', dest='TAR')
-
 parser.add_argument("-output_qasm_file_name", help="Name of the output file to write compiled circuit description in QASM format", type=str, default='QAOA.qasm', action='store', dest='OUT')
-
-#do the actual parsing and put them in an object - inputs
 ARGS = parser.parse_args()
 
 
